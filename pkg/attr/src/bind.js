350875411e4ff2f3d4771103de9c104ddefac80e
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-09-04 20:14:55 +0900

    wip: formatted

--- pkg/attr/src/bind.js
+++ pkg/attr/src/bind.js
@@ -1,6 +1,6 @@
 import { $ } from 'jsr:@ihasq/esptr@0.1.13';
 
 export const bind = $((value, ref) => {
-	ref.addEventListener("input", ({ target: { value: newValue } }) => $[value] = newValue);
-	value.watch(newValue => ref.value = newValue);
-})
+	ref.addEventListener('input', ({ target: { value: newValue } }) => $[value] = newValue);
+	value.watch((newValue) => ref.value = newValue);
+});
